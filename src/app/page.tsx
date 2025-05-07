51f282060c355c121b610e453b8f89d928ea8e48
fernteerana <teerana.b@ku.th>
2025-05-07 04:03:56 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Landdingpage from "@/components/Landdingpage";
+import React from "react";
+
+export default function home() {
+  return (
+    <div>
+      <Landdingpage />
+    </div>
+  );
+}
